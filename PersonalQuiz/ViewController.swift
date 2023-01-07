f4781263596b5dae630e2ae28336508f25e78bc3
Mykola-Kovalenko <44292547+Mykola-Kovalenko@users.noreply.github.com>
2023-01-07 15:58:39 +0700

    Initial Commit

---
+++ PersonalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Николай Коваленко on 07.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
