629711575929ff5a94eae1b8e58f6faa2cfee7a8
Mykola-Kovalenko <44292547+Mykola-Kovalenko@users.noreply.github.com>
2023-01-28 16:42:16 +0700

    done

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -19,12 +19,20 @@ class QuestionsViewController: UIViewController {
     
     @IBOutlet var rangedLabels: [UILabel]!
     @IBOutlet var rangeStackView: UIStackView!
-    @IBOutlet var rangeSlider: UISlider!
+    @IBOutlet var rangeSlider: UISlider! {
+        didSet {
+            let answerCount = Float(currentAnswers.count - 1)
+            rangeSlider.maximumValue = answerCount
+            rangeSlider.value = answerCount / 2
+        }
+    }
     
     @IBOutlet var progressView: UIProgressView!
     
     private let questions = Question.getQuestion()
     private var questionIndex = 0
+    private var answersChosen:[Answer] = []
+    
     private var currentAnswers: [Answer] {
         questions[questionIndex].answers
     }
@@ -37,20 +45,34 @@ class QuestionsViewController: UIViewController {
     
     
     @IBAction func singleButtonAnswerPressed(_ sender: UIButton) {
-        
-        
+        guard let buttonIndex = singleButtons.firstIndex(of: sender) else { return }
+        let answer = currentAnswers[buttonIndex]
+        answersChosen.append(answer)
+        nextQuestion()
     }
     
     @IBAction func multipleButtonAnswerPressed(_ sender: Any) {
-        
+        for(multipleSwitch, answer ) in zip(multipleSwitches, currentAnswers) {
+            if multipleSwitch.isOn {
+                answersChosen.append(answer)
+            }
+        }
+        nextQuestion()
     }
     
     @IBAction func rangeButtonAnswerPressed(_ sender: Any) {
-        
+        let index = lrintf(rangeSlider.value)
+        answersChosen.append(currentAnswers[index])
+        nextQuestion()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let answers = segue.destination as? ResultViewController else { return }
+        answers.current = answersChosen
+    }
     
 }
+
 // MARK: - Private Metods
 extension QuestionsViewController {
     private func updateUI() {
@@ -82,9 +104,9 @@ extension QuestionsViewController {
         case .single:
             showSingleStackView(with: currentAnswers)
         case .mulitple:
-            break
+            showMultipleStackView(with: currentAnswers)
         case .range:
-            break
+            showRangeStackView(with: currentAnswers)
         }
     }
     
@@ -95,8 +117,35 @@ extension QuestionsViewController {
             button.setTitle(answer.title, for: .normal)
             
         }
+    }
+    
+    private func showMultipleStackView(with answers: [Answer] ) {
+        multipleStackView.isHidden = false
         
+        for (labels, answers) in zip(multipleLabels, answers) {
+            labels.text = answers.title
+        }
+    }
+    
+    private func showRangeStackView(with answers: [Answer] ) {
+        rangeStackView.isHidden = false
         
+        rangedLabels.first?.text = answers.first?.title
+        rangedLabels.last?.text = answers.last?.title
+    }
+    
+    private func nextQuestion() {
+        questionIndex += 1
         
+        if questionIndex < questions.count {
+            updateUI()
+            return
+        }
+        performSegue(withIdentifier: "showResult", sender: nil)
     }
 }
+
+
+
+
+
--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -8,22 +8,40 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
+    
+    @IBOutlet var presonalIdentifcator: UILabel!
+    @IBOutlet var explanations: UILabel!
+    var current: [Answer]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
+        getResult()
         // Do any additional setup after loading the view.
+    }
+    private func getResult() {
+        var frequencyOfAnimals:[AnimalType: Int] = [:]
+        
+        let animals = current.map { $0.type }
+        for animal in animals {
+            frequencyOfAnimals[animal] = (frequencyOfAnimals[animal] ?? 0) + 1
+        }
+        
+        let sortedFrequencyOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
+        guard let mostFrequncyAnimal = sortedFrequencyOfAnimals.first?.key else { return }
+        
+        updateUI(with: mostFrequncyAnimal)
+    }
+    private func updateUI(with animal: AnimalType) {
+        presonalIdentifcator.text = "Вы - \(animal.rawValue)"
+        explanations.text = "\(animal.definition)"
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+        
     }
-    */
-
 }
